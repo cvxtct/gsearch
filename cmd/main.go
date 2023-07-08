828eb11f4a2a487a07f7ee5a5ab19a928f386886
Attila, Balázs <ifearnoghosts@gmail.com>
2023-07-08 11:14:19 +0200

    Remove goroutine

--- cmd/main.go
+++ cmd/main.go
@@ -1,42 +1,11 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
 	"log"
-	"os"
 	"runtime"
-	"strings"
-	"time"
 
 	"runtime/pprof"
-
-	"github.com/fatih/color"
 )
-
-// DocumentProcessor
-func (p *Project) documentProcessor(f string) bool {
-	// parse document
-	doc, err := p.parseDocument(f)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// append document to documents
-	p.documents = append(p.documents, doc)
-	// add document to index
-	p.idx.add(doc)
-
-	return true
-}
-
-// Runner is in charge to run Indexer in go routine
-// TODO FIX parallel11ism!
-func (p *Project) runner(boolChan chan bool) {
-	for _, f := range p.files {
-		indexed_file := p.documentProcessor(f)
-		boolChan <- indexed_file
-	}
-}
 
 func preFlightCheckOs() {
 	opsys := runtime.GOOS
@@ -68,68 +37,16 @@ func main() {
 	preFlightCheckOs()
 	log.Println("Pre flight checks done!")
 
-	// channel for sending back results of indexer
-	boolChan := make(chan bool)
-	defer close(boolChan)
+	p.dir = "/Users/attilabalazs/Projects/__GO__"
 
-	p.dir = "/Users/attilabalazs/Projects/"
 	p.readFileNames()
+	p.runIndex()
 
 	// TODOs
 	// cli program should store index on disk,
 	// create file time hashes
 	// recreate each file hash upon start,
 	// if change -> reindex
-	start := time.Now()
-	go p.runner(boolChan)
-	// Wait for Runner to finish.
-	for i := 0; i < len(p.files); i++ {
-		res := <-boolChan
-		if res {
-			log.Println("Item: ", p.files[i], " indexed!")
-		}
-		if i == len(p.files)-1 {
-			log.Println("All files are indexed!")
-			break
-		}
-	}
 
-
-	elapsed := time.Since(start)
-	log.Printf("Indexing took: %s", elapsed)
-
-	reader := bufio.NewReader(os.Stdin)
-
-	for {
-		// TODO make this bar dynamic
-		color.Yellow("--------------------- s e a r c h ---------------------")
-		color.Green("Documents indexed: %d", len(p.documents))
-		color.Green("Index size: %d token(s)", len(p.idx))
-		color.Blue(("Threads: %d\n"), threadProfile.Count())
-		fmt.Print("-> ")
-
-		text, _ := reader.ReadString('\n')
-		text = strings.Replace(text, "\n", "", -1)
-		p.query = text
-
-		start = time.Now()
-		matchedIDs := p.idx.search(p.query)
-		elapsed := time.Since(start)
-
-		if matchedIDs == nil {
-			color.Red("Term %s not in index!", p.query)
-			continue
-		}
-
-		log.Printf("Search found in %d document(s)", len(matchedIDs))
-
-		for _, id := range matchedIDs {
-			doc := p.documents[id]
-			// TODO make this dynamic
-			color.White("---------------------------------------------------")
-			fmt.Printf("[In: %s]\t\nText: %s\n", doc.FilePath, doc.Text)
-		}
-		color.White("---------------------------------------------------")
-		color.Yellow("Search took: %s", elapsed)
-	}
+	p.interact()
 }
